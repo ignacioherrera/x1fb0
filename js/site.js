4037fd49d807038ab2a432e98957589bd70ae16c
ignacio <iherreraaguero5@gmail.com>
2019-09-23 14:15:21 -0400

    cambios

--- js/site.js
+++ js/site.js
@@ -2,46 +2,23 @@ $(document).ready(function () {
     var clientHeight = $(window).height();
     var width = $(window).width();
     //Header al 100% de la pantalla del cliente
-    if (width >= 768) {
-        $('.fullHeightHeader').height(clientHeight);
-        // Fullpage lib
-        new fullpage('#fullpage', {
-            anchors: ['page1', 'page2', 'page3', 'page4'],
-            navigationTooltips: ['Home', 'Orders', 'Easy', 'Touch'],
-            css3: true,
-            scrollingSpeed: 1000,
-            navigationPosition: 'left',
-            navigation: true,
-            slidesNavigation: true,
-            responsiveHeight: 330,
-            dragAndMove: true,
-            dragAndMoveKey: 'YWx2YXJvdHJpZ28uY29tX0EyMlpISmhaMEZ1WkUxdmRtVT0wWUc=',
-            controlArrows: false
-        });
-    }
-    $(window).resize(function () {
-        clientHeight = $(window).height();
-        width = $(window).width();
-        $('.fullHeightHeader').height(clientHeight);
-        // Fullpage lib
-        new fullpage('#fullpage', {
-            anchors: ['page1', 'page2', 'page3', 'page4'],
-            navigationTooltips: ['Home', 'Orders', 'Easy', 'Touch'],
-            css3: true,
-            scrollingSpeed: 1000,
-            navigationPosition: 'left',
-            navigation: true,
-            slidesNavigation: true,
-            responsiveHeight: 330,
-            dragAndMove: true,
-            dragAndMoveKey: 'YWx2YXJvdHJpZ28uY29tX0EyMlpISmhaMEZ1WkUxdmRtVT0wWUc=',
-            controlArrows: false
-        });
-    });
-
+    $('.fullHeightHeader').height(clientHeight);
+    
+    
     $('#menu-button, #menu-close-button').on('click touchend', function (e) {
         e.preventDefault();
         $('body').toggleClass('pushed-right');
         $('#menu-button').toggleClass('is-active');
     });
+    if(width<= 767){
+        $(window).scroll(function() {    
+            var scroll = $(window).scrollTop();
+        
+            if (scroll > $('#change_header').offset().top) {
+                $(".header").addClass("change"); // you don't need to add a "." in before your class name
+            } else {
+                $(".header").removeClass("change");
+            }
+        });
+    }
 });
